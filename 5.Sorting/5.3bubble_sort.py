d8a96d53286bda113fdb03fa28ef1ca6cb59b799
Bibek227 <bibeksahoo447@gmail.com>
2025-03-20 11:00:56 +0530

    add bubble sort implementation

---
+++ 5.Sorting/5.3bubble_sort.py
@@ -0,0 +1,8 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+arr=[5,23,445,2,1,3]
+print(bubbleSort(arr))
